d3fb493ff3d9ddcb1d6721ec60a9167bd0ebda54
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2024-10-11 18:33:51 -0300

    fix

--- script/ubuntu/build.sh
+++ script/ubuntu/build.sh
@@ -4,11 +4,13 @@ set -euxo pipefail
 
 BASE_PATH="$(dirname "$0")/../.."
 
+
 rm --force --recursive "$BASE_PATH/build"
 rm --force "$BASE_PATH/src/main/c/frontend/lexical-analysis/FlexScanner.c"
 rm --force "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.c"
 rm --force "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.h"
 
+cd "$BASE_PATH"
 cmake -S . -B build
 cd build
 make
